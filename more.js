6a46eb7509f9ac18f3e450d0ad3a5ed543cc0caf
SuraiyaAkterTrishna <suraiyaaktertrishna@gmail.com>
2023-03-10 01:46:31 +0600

    toUpperCase()

--- more.js
+++ more.js
@@ -1,7 +1,9 @@
 const lyrics = 'Tumi bondhu kala Pakhi ami jeno ki Bosonto kale tomay bolte parini';
-const lyricsLowerCase = lyrics.toLowerCase();
+// const lyricsLowerCase = lyrics.toLowerCase();
+const lyricsLowerCase = lyrics.toUpperCase();
 const searchString = 'Pakhi';
-const searchStringLower = searchString.toLowerCase();
+// const searchStringLower = searchString.toLowerCase();
+const searchStringLower = searchString.toUpperCase();
 const doesExits = lyricsLowerCase.includes(searchStringLower);
 // const doesExits = lyrics.includes('Pakhi');
 console.log(doesExits);
