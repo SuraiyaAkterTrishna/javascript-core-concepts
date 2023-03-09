c64c1bfee674525a332af45e41570c6ef87507a7
SuraiyaAkterTrishna <suraiyaaktertrishna@gmail.com>
2023-03-09 18:34:10 +0600

    string is immutable

---
+++ string-immutable.js
@@ -0,0 +1,7 @@
+const myAddress = 'Khulna, Bangladesh';
+// console.log(myAddress.length);
+// console.log(myAddress[3]);
+const firstName = 'Hannan';
+const lastName = 'Poribahan';
+const fullName = firstName + lastName;
+console.log(fullName);
