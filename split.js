844dc5e2e85ccf50844cc4cbcda941802b6a2431
SuraiyaAkterTrishna <suraiyaaktertrishna@gmail.com>
2023-03-11 19:09:50 +0600

    charAt(index) will return the value of index 6, charCodeAt(index) will return the character code of index 6 letter's

--- split.js
+++
@@ -1,4 +0,0 @@
-// String.at()
-const lyrics = 'Tumi bondhu kala pakhi ami jano ki, boshonto kale tomay bolte parini';
-console.log(lyrics.at(6));
-console.log(lyrics.at(-6));
---
+++ string.js
@@ -0,0 +1,9 @@
+// string.at()
+const lyrics = 'Tumi bondhu kala pakhi ami jano ki, boshonto kale tomay bolte parini';
+// console.log(lyrics.at(6));
+// console.log(lyrics.at(-6));
+
+// charAt(index) will return the value of index 6, charCodeAt(index) will return the character code of index 6 letter's
+console.log(lyrics.charAt(6));
+console.log(lyrics.charCodeAt(6));
+
