2f07880b04523ddc044be40b986de1bb0fc0a5cc
SuraiyaAkterTrishna <suraiyaaktertrishna@gmail.com>
2023-03-11 19:55:25 +0600

    trim() trimStart() trimEnd()

---
+++ split.js
@@ -0,0 +1,6 @@
+const lyrics = 'Tumi bondhu kala pakhi ami jano ki, boshonto kale tomay bolte parini';
+const lyricsCopy = lyrics.split(''); //separates all the character of array
+// console.log(lyricsCopy);
+const lyricsUpdate = lyrics.split(' ');
+console.log(lyricsUpdate);
+console.log(lyricsUpdate[5]);
---
+++ trim.js
@@ -0,0 +1,9 @@
+const userName = '   Peter Rabbit   ';
+// trim()
+console.log(userName.trim());
+
+// trimEnd()
+console.log(userName.trimEnd());
+
+// trimStart()
+console.log(userName.trimStart());
