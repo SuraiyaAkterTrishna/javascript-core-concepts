c9720f8acb44299290c8b423acfe2f0855b2a0c7
SuraiyaAkterTrishna <suraiyaaktertrishna@gmail.com>
2023-03-11 19:14:08 +0600

    String.concat()

--- string.js
+++ string.js
@@ -4,6 +4,12 @@ const lyrics = 'Tumi bondhu kala pakhi ami jano ki, boshonto kale tomay bolte pa
 // console.log(lyrics.at(-6));
 
 // charAt(index) will return the value of index 6, charCodeAt(index) will return the character code of index 6 letter's
-console.log(lyrics.charAt(6));
-console.log(lyrics.charCodeAt(6));
+/* console.log(lyrics.charAt(6));
+console.log(lyrics.charCodeAt(6)); */
+
+// String.concat() will concat two string 
+const poem1 = 'asmanider dekhte jodi tomra sobe chao';
+const porm2 = 'rohimuddir sotto bari rosul pure jao';
+const poem = poem1.concat(' ',porm2);
+console.log(poem);
 
