bb01f8dd38242226c616dfe532b6700fdf95b2d9
SuraiyaAkterTrishna <suraiyaaktertrishna@gmail.com>
2023-03-11 22:31:58 +0600

    max for two values

--- max.js
+++ max.js
@@ -1,9 +1,29 @@
 // max from two values
 const jim = 84; 
 const dela = 77; 
-if(jim > dela) {
+
+// using if-else statement 
+/* if(jim > dela) {
     console.log("Jim will eat the cake");
 }
 else {
     console.log("Dela will eat the cake");
+} */
+
+// using function 
+/* function getMax(a, b) {
+    if(a > b){
+        return a;
+    }
+    else {
+        return b;
+    }
 }
+const largest = getMax(jim, dela);
+console.log(largest); */
+
+// using Math.max()
+
+const largest = Math.max(jim, dela);
+console.log(largest);
+
