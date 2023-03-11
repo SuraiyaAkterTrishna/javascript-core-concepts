c55453428eee19c9a80b3fb43d81f382a1bc11fb
SuraiyaAkterTrishna <suraiyaaktertrishna@gmail.com>
2023-03-11 22:24:54 +0600

    swap variables

--- swap.js
+++ swap.js
@@ -1,7 +1,7 @@
 // swap with a temp variable
 var a = 10; 
 var b = 7; 
-var temp = a; 
+var temp = a;
 a = b; 
 b = temp;
 // console.log(a, b);
@@ -12,10 +12,10 @@ var q = 10;
 p = p + q; 
 q = p - q;
 p = p - q;
-console.log(p, q);
+// console.log(p, q);
 
 //simply swap
 var x = 30; 
 var y = 50;
 [x , y] = [y , x];
-console.log(x, y);
+// console.log(x, y);
